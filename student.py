bcf7fc3629e8c23c1152d1287314702a3552ec5f
shutingst0 <shutingst@hotmail.com>
2026-05-24 01:54:53 +0000

    break out student class

---
+++ student.py
@@ -0,0 +1,30 @@
+class Student:
+    def __init__(self, student_id, name, email, password, subjects=None):
+        self.id = student_id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.subjects = subjects if subjects is not None else []
+
+    def average_mark(self):
+        if len(self.subjects) == 0:
+            return 0.0
+
+        total = 0
+        for subject in self.subjects:
+            total += subject["mark"]
+
+        return total / len(self.subjects)
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(d["id"], d["name"], d["email"], d["password"], d.get("subjects", []))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "subjects": self.subjects
+        }
--- uni_cli_app.py
+++ uni_cli_app.py
@@ -35,34 +35,7 @@ class Subject:
             "grade": self.grade
         }
 
-
-class Student:
-    def __init__(self, student_id, name, email, password, subjects=None):
-        self.id = student_id
-        self.name = name
-        self.email = email
-        self.password = password
-        self.subjects = subjects if subjects is not None else []
-
-    def average_mark(self):
-        if len(self.subjects) == 0:
-            return 0.0
-
-        total = 0
-        for subject in self.subjects:
-            total += subject["mark"]
-
-        return total / len(self.subjects)
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "email": self.email,
-            "password": self.password,
-            "subjects": self.subjects
-        }
-
+from student import Student
 
 class Database:
     def __init__(self, filename=DATA_FILE):
