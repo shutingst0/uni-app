a4750c68f5851d98d23a15d6f8135494b974b5ba
shutingst0 <shutingst@hotmail.com>
2026-05-24 08:39:07 +0000

    format student string output

--- student.py
+++ student.py
@@ -24,6 +24,19 @@ class Student:
         grade_average = grade_from_mark(mark_average)
         return {"mark_average": mark_average, "grade_average": grade_average}
 
+    def __str__(self):
+        result = self.get_result()
+        student_info = f"{self.name} :: {self.id} --> Email: {self.email} --> Grade Average: {result['grade_average']} --> Mark Average: {result['mark_average']:.2f}"
+
+        if len(self.subjects) == 0:
+            return student_info
+
+        subject_lines = ""
+        for subject in self.subjects:
+            subject_lines += f"\n  Subject-{subject['id']} --> Mark: {subject['mark']} --> Grade: {subject['grade']}"
+
+        return student_info + subject_lines
+
     def to_dict(self):
         result = self.get_result()
         return {
